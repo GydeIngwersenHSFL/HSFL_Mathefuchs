5df0bdf96f347ced2f552fa74b67dc17b14dfda4
GydeIngwersenHSFL <94675140+GydeIngwersenHSFL@users.noreply.github.com>
2021-12-04 19:05:46 +0100

    Added Unit Test Project

---
+++ MatheFuchsUnitTest/Program.cs
@@ -0,0 +1,74 @@
+﻿using Mathefuchs._3_Model;
+using System;
+using System.Collections.Generic;
+
+namespace MatheFuchsUnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 1; i++)
+            {
+                plusminustest(true, false, 10, 20, true);
+            }
+        }
+
+        private static void plusminustest( bool plus, bool minus, int nrOfTasks, int max, bool tenTransition){
+            Training training = new Training(plus, minus, nrOfTasks, max, tenTransition);
+
+            bool resultcheck = true;
+            bool withinRange = true;
+            bool differentTasks = true;
+            bool operatorsCorrect = true;
+
+            //are ther plus/minus operators?
+            int plusops = 0;
+            int minusops = 0;
+
+            List<string> n1s = new List<string>();
+            List<string> n2s = new List<string>();
+            
+            foreach (MTask task in training.tasks)
+            {
+                //operator count and result check
+                if (task.opstr == "+") { 
+                    plusops++; 
+                    if(Int16.Parse(task.n1) + Int16.Parse(task.n2) != task.result){ resultcheck = false; }
+                }
+                else { 
+                    minusops++;
+                    if (Int16.Parse(task.n1) - Int16.Parse(task.n2) != task.result) { resultcheck = false; }
+                }
+
+                //number check for max number
+                if(Int16.Parse(task.n1) > max || Int16.Parse(task.n2) > max|| task.result > max || task.result < 0) { withinRange = false; }
+
+                //check for different taks
+
+                if(n1s.Contains(task.n1) && n2s.Contains(task.n2)) { differentTasks = false; }
+
+                n1s.Add(task.n1);
+                n2s.Add(task.n2);
+            }
+
+            if (plus) { if (plusops <= 0) { operatorsCorrect = false; } }
+            if (minus) { if (minusops <= 0 ) { operatorsCorrect = false; } }
+            
+
+            Console.WriteLine($"Unit Test: \n" +
+                $"\nresults: {resultcheck}, " +
+                $"\nNrRange: {withinRange}, " +
+                $"\nDifferentTaks: {differentTasks}, " +
+                $"\nOperatorsCorrect: {operatorsCorrect}" +
+                $"\nplus : {plusops}, minus: {minusops}\n" +
+                $"{training.ToString()}"
+                );
+
+            
+
+        }
+
+
+    }
+}
---
+++ MatheFuchsUnitTest/Training.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathefuchs._3_Model
+{
+    public class Training
+    {
+        public bool plus { get; set; }
+        public bool minus { get; set; }
+        public int nrOfTasks { get; set; }
+        public int tasksSolved { get; set; }
+        public int tasksSolvedAtFirstTry { get; set; }
+        public int max { get; set; }
+        public bool tenTransition { get; set; }
+        public MTask[] tasks { get; set; }
+
+        public Training(bool plus, bool minus, int nrOfTasks, int max, bool tenTransition)
+        {
+            this.plus = plus;
+            this.minus = minus;
+            this.nrOfTasks = nrOfTasks;
+            this.max = max;
+            this.tenTransition = tenTransition;
+            tasksSolved = 0;
+            tasksSolvedAtFirstTry = 0;
+
+            Random rnd = new Random();
+
+            tasks = new MTask[nrOfTasks];
+            MTask t;
+            Console.WriteLine("before loop");
+            for (int i = 0; i < nrOfTasks; i++)
+            {
+                Console.WriteLine("loop");
+                if (plus && minus)
+                {
+                    int r = rnd.Next(1);
+                    if (r == 0) { t = generatePlusTask(max, tenTransition); }
+                    else { t = generateMinusTask(max, tenTransition);  }
+                }else if (plus) { t = generatePlusTask(max, tenTransition); }
+                 else { t = generateMinusTask(max, tenTransition); }
+
+                tasks[i] = t;
+            }
+        }
+
+        private MTask generatePlusTask(int max, bool tenTransition)
+        {
+            var rnd = new Random();
+            int num1 = rnd.Next(0, max);
+            int num2 = rnd.Next(0, max - num1);
+
+            return new MTask(num1, num2, Operator.Plus);
+        }
+
+        private MTask generateMinusTask(int max, bool tenTransition)
+        {
+            var rand = new Random();
+            int num1 = rand.Next(0, max);
+            int num2 = rand.Next(0+num1, max);
+
+            return new MTask(num1, num2, Operator.Minus);
+        }
+
+        public override string ToString()
+        {
+            string print = "";
+            foreach (MTask task in tasks){
+                print += task.ToString() + "\n";
+            }
+            return print;
+        }
+
+    }
+}
