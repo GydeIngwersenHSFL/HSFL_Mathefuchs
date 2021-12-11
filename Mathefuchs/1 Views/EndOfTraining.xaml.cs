1595bd1f73232f4181ff43d3b9b7842dd8ab03e1
Gydance <gyde.ingwersen@stud.hs-flensburg.de>
2021-12-11 10:18:18 +0100

    added minigames, deleted imports

--- Mathefuchs/1 Views/EndOfTraining.xaml.cs
+++ Mathefuchs/1 Views/EndOfTraining.xaml.cs
@@ -1,18 +1,6 @@
-﻿using Mathefuchs._2_Viewmodels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Mathefuchs._2_Viewmodels;
 
 namespace Mathefuchs._1_Views
 {
@@ -29,7 +17,7 @@ namespace Mathefuchs._1_Views
 
         public void Next_Click(object sender, RoutedEventArgs e)
         {
-            ViewModel.ChangePage(new Mathefuchs._1_Views.MainView(), Window.GetWindow(this));
+            ViewModel.ChangePage(new Mathefuchs._1_Views.Minigame(), Window.GetWindow(this));
         }
     }
 }
